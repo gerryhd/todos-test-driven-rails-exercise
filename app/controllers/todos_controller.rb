d508801ec88d5c9ab278877bff6e4c7231381050
Gerardo Hernandez <gerry_hd@live.com.mx>
2019-05-03 18:22:02 -0500

    Finished testing see own todo

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,22 @@
 class TodosController < ApplicationController
+  before_filter :authenticate
   def index
+    @todos = Todo.where(email: session[:current_email])
+  end
+
+  def new
+    @todo = Todo.new
+  end
+
+  def create
+    Todo.create(todo_params.merge(email: session[:current_email]))
+    redirect_to todos_path
+  end
+
+  private
+
+  def todo_params
+    params.require(:todo).permit(:title)
   end
 
 end
---
+++ db/migrate/20190503224831_add_email_to_todos.rb
@@ -0,0 +1,6 @@
+class AddEmailToTodos < ActiveRecord::Migration[5.0]
+  def change
+    add_column :todos, :email, :string
+  end
+
+end
